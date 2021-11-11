f656e8863f2840ce938cb22a39f9a54589b53084
stas662 <92520407+stas662@users.noreply.github.com>
2021-11-12 01:02:23 +0200

    Add files via upload

---
+++ js/homework_8_part_1.js
@@ -0,0 +1,81 @@
+//Создаём элементы с голубым цветом размером 50х50`даём отсуп и выравниваем по центру страницы
+function createSquare() {
+
+	let divSquare = document.createElement('div');
+	document.querySelector('#square');
+	divSquare.setAttribute('id', 'square');
+
+	divSquare.style.width = '50px';
+	divSquare.style.height = '50px';
+	divSquare.style.background = '#87CEEB';
+	divSquare.style.marginBottom = '5px';
+	divSquare.style.marginLeft = 'auto';
+	divSquare.style.marginRight = 'auto';
+
+	document.body.append(divSquare);
+
+}
+
+
+//Перекрашиваем все элементы в зелёный цвет и меняем размер 100х100
+function changeСolor() {
+	let id = document.querySelectorAll('#square');
+	for (let i = 0; i < id.length; i++) {
+		id[i].style.background = '#008000';
+		id[i].style.width = '100px'; 
+		id[i].style.height = '100px';
+	}
+}
+
+
+//Перекрашиваем все элементы в жёлтый, но каждый 3 элемент делаем красным
+function changeElementColor() {
+	let id = document.querySelectorAll('#square');
+	for (let i = 0; i < id.length; i++) {
+		id[i].style.background = '#FFFF00';
+		
+		if (((i + 1) % 3 === 0) && (i !== 0)) {
+			id[i].style.background = '#FF0000';
+		}
+
+	}
+}
+
+
+//Удаляем созданные элементы и устанавливаем чёрный фон
+function clearElements() {
+	document.body.style.background = '#000000';
+	let id = document.querySelectorAll('#square')
+	for (let i = 0; i < id.length; i++) {
+		id[i].remove();
+	}
+	
+}
+
+
+//Для проверки входных параметров
+let result = false;
+
+//Вызов функций с таймером
+function getSetTimeout(item, time) {
+	
+	if ((typeof(item) !== 'function') || (typeof(time) !== 'number') || (result)) {
+		result = true;
+		return null;
+	}
+
+	result = false;
+
+	setTimeout( function() {
+		for (let i = 0; i < 10; i++) {
+			item();
+		}
+
+	}, time * 1000);
+}
+
+
+getSetTimeout(createSquare, 3);
+getSetTimeout(changeСolor, 6);
+getSetTimeout(changeElementColor, 9);
+getSetTimeout(clearElements, 12);
---
+++ js/homework_8_part_2.js
@@ -0,0 +1,77 @@
+let arrayColor = ['#808080', '#000000', '#008000', '#0000FF', '#FF0000', '#FFFF00', '#00FFFF', '#ADFF2F', '#FFA500'];
+let arrayNameColor = ['Серый', 'Чёрный', 'Зелёный', 'Синий', 'Красный', 'Жёлтый', 'Бирюзовый', 'Жёлто-зелёный', 'Оранжевый'];
+
+//Функия, которая возвращает случае целое число от min до max
+function random(min, max) {
+	if (isNaN(min) || isNaN(max)) {
+		return null;
+	}
+
+	let result = 0;
+	let randomNumber = Math.random();
+
+	result = (max - min) * randomNumber + min;
+	return Math.floor(result);
+}
+
+//Горизонтальное расположение двух блоков, что бы лучше их видеть
+let div = document.createElement('div'); 
+document.body.append(div);
+div.style.display = 'flex';
+
+//Что бы цвета не повторялись
+let duplicate = null;
+
+//Создаём элемент прямоугольник
+function createRectangle() {
+	
+	let divRectangle = document.createElement('div');
+	document.querySelector('#rectangle');
+	divRectangle.setAttribute('id', 'rectangle');
+
+	let widthRectangle = 0;
+	let heightRectangle = 0;
+
+	//Создаём и проверяем прямоугольник
+	while(widthRectangle === heightRectangle) {
+		widthRectangle = random(50, 200);
+		heightRectangle = random(50, 200);
+	}
+
+	let randomColorNumber = random(0, arrayColor.length);
+	
+	if (randomColorNumber === duplicate) {
+		console.log('1')
+		randomColorNumber = random(0, arrayColor.length);
+	}
+
+	duplicate = randomColorNumber;
+
+	divRectangle.style.width = widthRectangle + 'px';
+	divRectangle.style.height = heightRectangle + 'px';
+	divRectangle.style.background = arrayColor[randomColorNumber];
+	divRectangle.style.marginLeft = 'auto';
+	divRectangle.style.marginRight = 'auto';
+
+	
+	div.append(divRectangle);
+
+
+	//Наведении курсора мыши
+	divRectangle.onmouseover = function() {
+		console.log(`Ширина = ${this.style.width}, Высота = ${this.style.height}`);
+	}
+
+	//Клик по элементу
+	divRectangle.onclick = function() {
+		alert(`Цвет этого прямоугольника ${arrayNameColor[randomColorNumber]}`)
+	}
+
+
+}
+
+
+for (let i = 0; i < 2; i++) {
+	createRectangle();
+	
+}
