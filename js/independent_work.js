513cde9b2ee79dcbe9391e21a21d8a4bed072dfe
stas662 <92520407+stas662@users.noreply.github.com>
2021-11-13 22:34:52 +0200

    Update independent_work.js

--- js/independent_work.js
+++ js/independent_work.js
@@ -22,7 +22,26 @@ function changeСolor() {
 		id[i].style.display = 'flex';
 		id[i].style.flexDirection = 'column';
 		id[i].style.justifyContent = 'center';
-		id[i].style.textAlign = 'center';
+document.body.style.display = 'flex';
+
+function createSquare() {
+	let divSquare = document.createElement('div');
+	document.querySelector('#square');
+	divSquare.setAttribute('id', 'square');
+
+	divSquare.style.minWidth = '70px';
+	divSquare.style.minHeight = '70px';
+	divSquare.style.background = '#87CEEB';
+	divSquare.style.marginRight = '5px'
+
+	document.body.append(divSquare);
+
+}
+
+function changeСolor() {
+	let id = document.querySelectorAll('#square');
+	for (let i = 0; i < id.length; i++) {
+		id[i].style.background = '#800080';
 		id[i].style.minWidth = '10px'; 
 		id[i].style.minHeight = '10px';
 	}
@@ -40,14 +59,13 @@ function changeElementColor() {
 	let id = document.querySelectorAll('#square');
 	for (let i = 0; i < id.length; i++) {
 		id[i].style.background = '#FFFF00';
-		id[i].style.color = '#000000';
 		
 		if (((i + 1) % 3 === 0) && (i !== 0)) {
 			id[i].style.background = '#FF0000';
-			id[i].style.color = '#000000';
 		}
+
 		if (((i + 1) % 15 === 0) && (i !== 0)) {
-			id[i].style.background = 'linear-gradient(to right bottom, transparent 50%, #78866b 50%) left / 50% 100% no-repeat, linear-gradient(to left bottom, transparent 50%, #78866b 50%) right / 50% 100% no-repeat'
+			id[i].style.background = 'linear-gradient(to right bottom, transparent 50%, #78866b 50%) left / 50% 100% no-repeat, linear-gradient(to left bottom, transparent 50%, #78866b 50%) right / 51% 100% no-repeat'
 		}
 	}
 }
